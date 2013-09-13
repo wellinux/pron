866f499f092cc191372f528c1741733a990c5b36
Wellinux <wjas@globo.com>
2013-09-12 22:11:31 -0300

    ajuste asset precompile

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,9 @@ module Prontusoft
     config.assets.precompile += %w(event_calendar.css)
     config.assets.precompile += %w(impressao.css.scss)
     config.assets.precompile += %w(record.css.scss)
-    
+
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
