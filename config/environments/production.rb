08630412a11952318dab8f2410ff4522a6ad5c5c
Wellinux <wjas@globo.com>
2013-09-13 18:05:31 -0300

    mais um ajuste no precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,8 @@ Prontusoft::Application.configure do
   #config.assets.precompile += %w(event_calendar.css)
   #config.assets.precompile += %w(impressao.css.scss)
   #config.assets.precompile += %w(record.css.scss)
-  #config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = false
+  config.assets.compile = false
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -67,6 +68,4 @@ Prontusoft::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  config.assets.precompile += %w(active_admin.js active_admin.css active_admin/print.css)
 end
